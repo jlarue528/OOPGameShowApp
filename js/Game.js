8899b59ff4ce237403fc55365c5af9712e15d323
Jennifer LaRue <jennifer@photoshelter.com>
2021-02-25 07:25:51 -0500

    step 5 complete

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,25 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor () {
+        this.missed = 0;
+        this.phrases = [
+            {phrase: 'you can do it'}, 
+            {phrase: 'do not give up'}, 
+            {phrase: 'believe in yourself'}, 
+            {phrase: 'you are amazing'}, 
+            {phrase: 'love yourself'}
+        ];
+        this.active = null;
+    }
+
+    getRandomPhrase () {
+        const phrases = this.phrases;
+
+        let randomNumber = Math.floor(Math.random() * phrases.length);
+        let randomPhrase = this.phrases[randomNumber];
+        return randomPhrase
+    }
+ }
--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+//  const phrase = new Phrase();
+//  const game = new Game();
+
+// const phrase = new Phrase('Life is Like A Box Of Chocolates');
+// console.log(`Phrase - phrase: ${phrase.phrase}`)
+
+// const game = new Game();
+
+// game.phrases.forEach((phrase, index) => {
+//     console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+// });
+
+const logPhrase = (phrase) => {
+    console.log(`Phrase - phrase: `, phrase.phrase);
+};
+
+const game = new Game();
+
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+
+// logPhrase(game.getRandomPhrase);
