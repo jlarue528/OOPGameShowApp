e46053195d41522b1387f2561e242d1723736d5e
Jennifer LaRue <jennifer@photoshelter.com>
2021-02-24 21:33:47 -0500

    restart

--- js/Game.js
+++ js/Game.js
@@ -1,31 +1,3 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
-
- class Game {
-    constructor () {
-        this.missed = 0;
-        this.phrases = [
-            {
-                phrase: 'You are simply the best'
-            }, 
-            {
-                phrase: 'Life is a garden, dig it'
-            },
-            {
-                phrase: 'Life is beautiful'
-            },
-            {
-                phrase: 'Believe in yourself'
-            },
-            {
-                phrase: 'Never give up, never surrender'
-            }
-        ];
-        this.activePhrase = null;
-    }
-    
-    getRandomPhrase() {
-        return this.phrases[Math.floor(Math.random() * this.phrases.length)];
-    }
- }
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,25 +1,3 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
-
- class Phrase {
-     constructor (phrase) {
-        this.phrase = phrase;
-     }
-
-     addPhraseToDisplay() {
-        const ul = document.querySelector('#phrase ul');
-
-        for(let i = 0; i < this.phrase.length; i++) {
-            let li = document.createElement('li');
-            li.innerHTML = `${this.phrase[i]}`;
-            if( this.phrase[i] === ' ') {
-                li.className = 'space';
-            } else {
-                li.className += `hide letter ${this.phrase[i]}`
-            }
-
-            ul.appendChild(li);
-        }
-     }
- }
