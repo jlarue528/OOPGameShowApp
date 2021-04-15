d974f7c7ac3b41d9438a65f48d6b03e304e9e696
Jennifer LaRue <jennifer@photoshelter.com>
2021-04-15 16:01:52 -0400

    remove life method

--- js/Game.js
+++ js/Game.js
@@ -32,7 +32,6 @@ class Game {
         startScreenOverlay.style.visibility = 'hidden';
 
         this.activePhrase = this.getRandomPhrase();
-        console.log(this.activePhrase);
         this.activePhrase.addPhraseToDisplay();
     }
 
@@ -42,23 +41,61 @@ class Game {
     // }
 
     checkForWin() {
-            const activeGamePhraseLength = this.activePhrase.phrase.length;
-            const letterBox = document.querySelectorAll('#phrase ul li');
-            let exposedLetters = [];
-            let hiddenLetters = [];
-            for(let i = 0; i < letterBox.length; i++) {
-
-                if(letterBox[i].className.startsWith('show') || letterBox[i].className.startsWith('space')) {
-                    exposedLetters.push(letterBox[i]);
-                } else {
-                    hiddenLetters.push(letterBox[i]);
-                }
-            }
-            
-            if(exposedLetters.length === activeGamePhraseLength) {
-                return true;
+        const activeGamePhraseLength = this.activePhrase.phrase.length;
+        const letterBox = document.querySelectorAll('#phrase ul li');
+        let exposedLetters = [];
+        let hiddenLetters = [];
+        for(let i = 0; i < letterBox.length; i++) {
+            if(letterBox[i].className.startsWith('show') || letterBox[i].className.startsWith('space')) {
+                exposedLetters.push(letterBox[i]);
             } else {
-                return false;
+                hiddenLetters.push(letterBox[i]);
             }
         }
+            
+        if(exposedLetters.length === activeGamePhraseLength) {
+            return true;
+        } else {
+            return false;
+        }
     }
+
+    removeLife() {
+        const lives = document.querySelectorAll('#scoreboard ol li img');
+        let activeLives = [];
+        let usedLives = [];
+
+        for(let i = 0; i < lives.length; i++) {
+            if(lives[i].src === 'file:///Users/jennifer/Desktop/OOPGameShowApp/OOPGameShowApp/images/liveHeart.png') {
+                activeLives.push(lives[i]);
+            } else {
+                usedLives.push(lives[i]);
+            }
+        }
+
+        if(this.missed === 5 || activeLives.length === 0) {
+            //GAME OVER METHOD
+            console.log('gameOver');
+        } else {
+            activeLives[0].src = 'file:///Users/jennifer/Desktop/OOPGameShowApp/OOPGameShowApp/images/lostHeart.png';
+            this.missed += 1;
+        }
+        console.log(this.missed);
+    }
+
+    gameOver() {
+        const startScreenOverlay = document.getElementById('overlay');
+        startScreenOverlay.style.visibility = 'hidden';
+
+    }
+}
+
+    /** 
+* Increases the value of the missed property 
+* Removes a life from the scoreboard 
+* Checks if player has remaining lives and ends game if player is out */ 
+
+// removeLife()`: This method removes a life from the scoreboard, by replacing one of the `liveHeart.png` images with a `lostHeart.png` image 
+// (found in the `images` folder) and increments the `missed` property. If the player has five missed guesses 
+// (i.e they're out of lives), 
+// then end the game by calling the `gameOver()` method. 
